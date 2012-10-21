662d326ec0821fdd88e2828dfaef2cd8f06bee57
Roberto Luis <roberto.luis@rlbisbe.net>
2012-10-21 23:14:36 +0200

    First commit

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleObjectSaving
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PictureURL { get; set; }
+        public Person Mother { get; set; }
+        public Person Father { get; set; }
+    }
+}
